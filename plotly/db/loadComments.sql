4eab0fd07cd4b3a975bd98c70d42679f6746581a
eyyeung <eyanyeung@gmail.com>
2020-10-13 23:57:15 -0400

    add plotly- the dash app folder

---
+++ plotly/db/loadComments.sql
@@ -0,0 +1,21 @@
+-- drop existing table
+DROP TABLE IF EXISTS comments;
+
+.separator "|"
+-- define table
+CREATE TABLE comments (
+    ID INT,
+    COMMENT_ID TEXT,
+    COMMENT_TEXT TEXT,
+    ENTITY1 TEXT,
+    ENTITY2 TEXT,
+    ASPECT TEXT,
+    DIRECTION TEXT
+);
+
+
+.import rubber_comparison_output_fixed2.csv comments
+.headers off
+DELETE FROM comments WHERE aspect='aspect';
+SELECT COUNT(*) from comments;
+
